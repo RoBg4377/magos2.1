206f120a60317672f3a1a9b874cce3773a58d9ff
RodrigoBG <rhobg4377@gmail.com>
2022-07-07 15:01:54 -0300

    Segunda versión de página. con alguna interacción

---
+++ js/efectos.js
@@ -0,0 +1,56 @@
+
+function mostrarMenu() {
+  document.getElementById('navegacion').style.display = 'block';
+}
+function cerrarMenu() {
+  document.getElementById('navegacion').style.display = 'none';
+}
+
+function mostrarMenu2() {
+  document.getElementById('navegacion2').style.display = 'block';
+}
+function cerrarMenu2() {
+  document.getElementById('navegacion2').style.display = 'none';
+}
+
+
+function mostrarContenedor() {
+  document.getElementById('contenedorA').style.display = 'block';
+}
+function cerrarContenedor() {
+  document.getElementById('contenedorA').style.display = 'none';
+}
+
+
+function mostrarContenedorB() {
+  document.getElementById('contenedorB').style.display = 'block';
+}
+function cerrarContenedorB() {
+  document.getElementById('contenedorB').style.display = 'none';
+}
+
+function mostrarContenedorC() {
+  document.getElementById('contenedorC').style.display = 'block';
+}
+function cerrarContenedorC() {
+  document.getElementById('contenedorC').style.display = 'none';
+}
+// funcion para magos
+function mostrarMago1() {
+  document.getElementById('mago1').style.display = 'block';
+}
+function cerrarMago1() {
+  document.getElementById('mago1').style.display = 'none';
+}
+function mostrarMago1() {
+  document.getElementById('mago1').style.display = 'block';
+}
+function cerrarMago1() {
+  document.getElementById('mago1').style.display = 'none';
+}
+function mostrarMago2() {
+  document.getElementById('mago2').style.display = 'block';
+}
+function cerrarMago2() {
+  document.getElementById('mago2').style.display = 'none';
+}
